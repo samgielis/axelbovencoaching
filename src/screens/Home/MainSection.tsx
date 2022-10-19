82c28d31e83e260686dd2d6f704cd1e08f4265ff
Sam Gielis <sam.gielis@trustedfamily.net>
2022-10-19 21:36:32 +0200

    Use new man image + filtering
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/screens/Home/MainSection.tsx
+++ src/screens/Home/MainSection.tsx
@@ -3,8 +3,21 @@ import { useIsMobile } from "../../hooks/useIsMobile"
 
 export const MainSection = () => {
     const isMobile = useIsMobile();
-    return <Box w='100%' position='relative' bgImage={'/main.jpg'} h='80vh' minH='80vh' color='white' backgroundPosition='center' backgroundSize='cover'>
-    <Container maxW='container.xl' minH='80vh' h='100%'>
+    const height =  isMobile ? '55vh' : '85vh'
+    return <Box w='100%' position='relative'  h={height} minH={height} color='white' 
+    _before={{
+        h:'100%',
+        w: '100%',
+        bgImage: '/main.jpg',
+        filter: "brightness(.9)",
+        content: '""',
+        pos:'absolute',
+        backgroundSize: 'cover',
+        backgroundPosition: 'top',
+        zIndex: -1,
+        opacity: .9
+    }}>
+    <Container maxW='container.xl' h='100%'>
         <Center w='100%' h='100%'>
             <VStack spacing={5} textAlign='center'>
                 <Heading size='4xl' fontSize={isMobile ? '7xl' : '9xl'} fontWeight='light' textTransform='uppercase'>Axel Boven</Heading>
