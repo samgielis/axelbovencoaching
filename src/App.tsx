852bbd46d3f87e5bf3bad2dcecf5f610df306571
Sam Gielis <sam.gielis@trustedfamily.net>
2022-10-19 21:06:13 +0200

    Add Economica as Heading font
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import {
   ChakraProvider,
-  theme,
 } from "@chakra-ui/react"
 import { ScreenRouter } from "./ScreenRouter"
+import { Helmet } from "react-helmet";
+import theme from "./theme";
 
 export const App = () => (
-  <ChakraProvider theme={theme} resetCSS>
-    <ScreenRouter />
-  </ChakraProvider>
+  <>
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+      <link href="https://fonts.googleapis.com/css2?family=Economica:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+    </Helmet>
+    <ChakraProvider theme={theme} resetCSS>
+      <ScreenRouter />
+    </ChakraProvider>
+  </>
 )
