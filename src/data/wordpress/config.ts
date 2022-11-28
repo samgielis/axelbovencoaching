810c75f8454a0ae3e3af142741a2aba526655e66
Sam Gielis <sam.gielis@trustedfamily.net>
2022-11-28 20:27:32 +0100

    Update wordpress endpoint to Axel's site
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/data/wordpress/config.ts
+++ src/data/wordpress/config.ts
@@ -1,4 +1,4 @@
-const blogId = "samgielis.wordpress.com";
+const blogId = "coaching942.wordpress.com";
 export const WPConfig = {
   blogId,
   apiEndpoint: `https://public-api.wordpress.com/rest/v1.1/sites/${blogId}`,
