3ab6c6c5d03fcddcbe1f373fb9da4f9ce7ee9f03
Sam Gielis <sam.gielis@trustedfamily.net>
2022-12-02 19:52:56 +0100

    padding iso margin
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,4 +1,4 @@
 import { Container } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
-export const PageContainer = (props: PropsWithChildren) => <Container maxWidth={"container.md"} my={40} {...props}/>
+export const PageContainer = (props: PropsWithChildren) => <Container maxWidth={"container.md"} py={40} {...props}/>
