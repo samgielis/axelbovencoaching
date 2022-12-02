f3ffe160139d4aa7f5ac955dc67f877530f400c4
Sam Gielis <sam.gielis@trustedfamily.net>
2022-12-02 18:58:53 +0100

    Set post metatitle to post title
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/screens/Post/index.tsx
+++ src/screens/Post/index.tsx
@@ -1,61 +1,67 @@
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  Heading,
-  Stack,
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    Heading,
+    Stack,
 } from "@chakra-ui/react";
 import { useLoadPost } from "../../data/wordpress/loaders/useLoadPost";
 import { PostLoadingPlaceholder } from "./PostLoadingPlaceholder";
 import "./post.css";
+import { Helmet } from "react-helmet-async";
 
 interface PostProps {
-  postId: string;
-  category: {
-    name: string;
-    path: string;
-  };
+    postId: string;
+    category: {
+        name: string;
+        path: string;
+    };
 }
 
 export const Post = ({ postId, category }: PostProps) => {
-  const { isLoading, post } = useLoadPost(postId);
+    const { isLoading, post } = useLoadPost(postId);
 
-  if (isLoading) {
-    return <PostLoadingPlaceholder />;
-  }
+    if (isLoading) {
+        return <PostLoadingPlaceholder />;
+    }
 
-  if (!post) {
-    return <p>Could not find post</p>;
-  }
+    if (!post) {
+        return <p>Could not find post</p>;
+    }
 
-  return (
-    <Stack spacing={10}>
-      <Breadcrumb
-        colorScheme={"themeGreen"}
-        fontSize="lg"
-        spacing="8px"
-        separator={<ChevronRightIcon color="green.500" />}
-      >
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">Home</BreadcrumbLink>
-        </BreadcrumbItem>
+    return (
+        <>
+            <Helmet>
+                <title>{`${post.title} - Axel Boven Coaching`}</title>
+            </Helmet>
+            <Stack spacing={10}>
+                <Breadcrumb
+                    colorScheme={"themeGreen"}
+                    fontSize="lg"
+                    spacing="8px"
+                    separator={<ChevronRightIcon color="green.500" />}
+                >
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                    </BreadcrumbItem>
 
-        <BreadcrumbItem>
-          <BreadcrumbLink href={category.path}>{category.name}</BreadcrumbLink>
-        </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href={category.path}>{category.name}</BreadcrumbLink>
+                    </BreadcrumbItem>
 
-        <BreadcrumbItem isCurrentPage>
-          <BreadcrumbLink>{post.title}</BreadcrumbLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
-      <Heading as="h1" size="3xl">
-        {post.title}
-      </Heading>
-      <div
-        className="wp-post"
-        dangerouslySetInnerHTML={{ __html: post.content }}
-      />
-    </Stack>
-  );
+                    <BreadcrumbItem isCurrentPage>
+                        <BreadcrumbLink>{post.title}</BreadcrumbLink>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+                <Heading as="h1" size="3xl">
+                    {post.title}
+                </Heading>
+                <div
+                    className="wp-post"
+                    dangerouslySetInnerHTML={{ __html: post.content }}
+                />
+            </Stack>
+        </>
+    );
 };
