1dc5f0c73c9248b9a855c192d5d47dbe276da88b
Sam Gielis <sam.gielis@trustedfamily.net>
2022-12-08 18:02:00 +0100

    Aanbod overview
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/screens/Aanbod/screens/Overview/index.tsx
+++ src/screens/Aanbod/screens/Overview/index.tsx
@@ -1,7 +1,11 @@
-import { Stack } from "@chakra-ui/react";
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Heading, Spacer, Stack } from "@chakra-ui/react";
 import { Helmet } from "react-helmet-async";
+import { PageContainer } from "../../../../components/PageContainer";
 import { QuoteWithCTASection } from "../../../../components/QuoteWithCTASection";
-import { PlaceholderScreen } from "../../../PlaceholderScreen";
+import { usePreloadCategory } from "../../../../data/wordpress/loaders/usePreloadCategory";
+import { useIsMobile } from "../../../../hooks/useIsMobile";
+import { OfferingCard } from "../../../Home/components/OfferingCard";
+import { PostLoadingPlaceholder } from "../../../Post/PostLoadingPlaceholder";
 
 export const Overview = () => {
   return (
@@ -10,13 +14,99 @@ export const Overview = () => {
         <title>Aanbod - Axel Boven Coaching</title>
       </Helmet>
       <Stack spacing={0} w="full">
-        <QuoteWithCTASection title={'"The fire, it speaks."'} />
-        <PlaceholderScreen title="Aanbod">
-          We zijn momenteel nog druk bezig met het ontwikkelen van mijn website.
-          Je vindt hier binnenkort meer over de trajecten en workshops die ik
-          aanbied. Een voorsmaakje vind je al op de homepage.
-        </PlaceholderScreen>
+        <QuoteWithCTASection
+          title={
+            '"The fire, it speaks."'
+          }
+        />
+        <Box bg="themeGreen.50" minH="100vh">
+          <PageContainer hasCustomHeading>
+            <AanbodContent />
+          </PageContainer>
+        </Box>
       </Stack>
     </>
+  );
+};
+
+const AanbodContent = () => {
+  const { isLoading, category } = usePreloadCategory("aanbod");
+  const isMobile = useIsMobile();
+
+  if (isLoading) {
+    return <PostLoadingPlaceholder />;
+  }
+
+  const workshops = category?.posts.filter(post => !!Object.keys(post.tags).find(
+    (tag) => tag == "Workshop"
+  )) || [];
+
+  const trajecten = category?.posts.filter(post => !!Object.keys(post.tags).find(
+    (tag) => tag == "Coaching traject"
+  )) || [];
+
+  return (
+    <Stack spacing={10}>
+      <Heading size="3xl">Aanbod</Heading>
+
+      <Accordion allowToggle allowMultiple defaultIndex={[0,1]} >
+        <AccordionItem border='none'>
+          <AccordionButton px={1}>
+            <Heading size="lg">Trajecten</Heading>
+            <Spacer />
+
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel px={0} pb={20}>
+            <Stack spacing={4}>
+              {trajecten.map((post) => {
+
+                return (
+                  <OfferingCard
+                    href={`/aanbod/${post.slug}`}
+                    key={post.ID}
+                    type={"TRAJECT"}
+                    title={post.title}
+                    imageSrc={post.featured_image}
+                    summary={post.excerpt}
+                    isMobile={isMobile}
+                  />
+                );
+              })}
+            </Stack>
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem border='none'>
+          <AccordionButton px={1}>
+            <Heading size="lg">Workshops</Heading>
+            <Spacer />
+
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel px={0} pb={4}>
+            <Stack spacing={4}>
+
+              {workshops.map((post) => {
+
+                return (
+                  <OfferingCard
+                    href={`/aanbod/${post.slug}`}
+                    key={post.ID}
+                    type={"WORKSHOP"}
+                    title={post.title}
+                    imageSrc={post.featured_image}
+                    summary={post.excerpt}
+                    isMobile={isMobile}
+
+                  />
+                );
+              })}
+            </Stack>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+
+    </Stack>
   );
 };
