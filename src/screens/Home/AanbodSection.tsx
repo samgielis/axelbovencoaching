748a2af53e601cbd32728417507ac8d57c412223
Sam Gielis <sam.gielis@trustedfamily.net>
2022-12-08 17:55:56 +0100

    Mobile optimisation OfferingCard
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/screens/Home/AanbodSection.tsx
+++ src/screens/Home/AanbodSection.tsx
@@ -1,19 +1,19 @@
 import { Box, HStack } from "@chakra-ui/react";
 import { TitledSection } from "../../components/TitledSection";
 import { usePreloadCategory } from "../../data/wordpress/loaders/usePreloadCategory";
+import { useIsMobile } from "../../hooks/useIsMobile";
 import { OfferingCard } from "./components/OfferingCard";
 
 export const AanbodSection = () => {
   const { isLoading, category } = usePreloadCategory("aanbod");
-
+  const isMobile = useIsMobile();
   if (isLoading) return null;
 
   return (
     <TitledSection bgColor="themeGreen.400" title={"Aanbod"} useAsScrollbox>
       <Box w="full" overflow={"auto"}>
-        <HStack spacing={8} alignItems="stretch">
+        <HStack spacing={4} alignItems="stretch">
           {category?.posts.map((post) => {
-            console.log(post.tags);
             const type = Object.keys(post.tags).find(
               (tag) => tag === "Workshop"
             )
@@ -27,6 +27,7 @@ export const AanbodSection = () => {
                 title={post.title}
                 imageSrc={post.featured_image}
                 summary={post.excerpt}
+                isMobile={isMobile}
               />
             );
           })}
--- src/screens/Home/components/OfferingCard.tsx
+++ src/screens/Home/components/OfferingCard.tsx
@@ -14,7 +14,11 @@ import { Link } from "react-router-dom";
 
 type OfferingType = "WORKSHOP" | "TRAJECT";
 
-interface OfferingCardProps {
+interface OfferingCardProps extends BaseOfferingCardProps{
+  isMobile?: boolean;
+}
+
+interface BaseOfferingCardProps {
   type: OfferingType;
   title: string;
   imageSrc: string;
@@ -22,7 +26,58 @@ interface OfferingCardProps {
   href: string;
 }
 
-export const OfferingCard = ({
+export const OfferingCard = ({isMobile, ...props}: OfferingCardProps) => {
+  if (isMobile) {
+    return <MobileOfferingCard {...props}/>
+  }
+  return <DesktopOfferingCard {...props}/>
+}
+
+const MobileOfferingCard = ({
+  type,
+  title,
+  summary,
+  imageSrc,
+  href,
+}: OfferingCardProps) => {
+  return (
+    <Box
+      minW="300px"
+      borderRadius={"md"}
+      boxShadow="md"
+      overflow="hidden"
+      bg="white"
+      flex={1}
+    >
+      <Stack spacing={0} alignItems="stretch" h="100%">
+        <Image maxHeight='200px' src={imageSrc} alt={title} objectFit="cover" />
+        <Stack p={4} flex={1}>
+          <Stack>
+            <OfferingTypeLabel type={type} />
+            <Heading>{title}</Heading>
+          </Stack>
+
+          <Spacer />
+
+          <Text dangerouslySetInnerHTML={{ __html: summary }} />
+
+          <Spacer />
+
+          <ButtonGroup size="sm" colorScheme="themeGreen">
+            <Button as={Link} to={href} flex={0.5}>
+              Starten
+            </Button>
+            <Button as={Link} to={href} flex={0.5} variant="link">
+              Meer leren
+            </Button>
+          </ButtonGroup>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+const DesktopOfferingCard = ({
   type,
   title,
   summary,
