8b1f0e65f3444b597e9c7e6402f6b1faa653cb00
Sam Gielis <samgielis@gmail.com>
2023-05-10 23:03:12 +0200

    Fix missing dependencies, crossorigin

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ export const App = () => (
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
-        crossOrigin="true"
       />
       <link
         href="https://fonts.googleapis.com/css2?family=Economica:ital,wght@0,400;0,700;1,400;1,700&display=swap"
