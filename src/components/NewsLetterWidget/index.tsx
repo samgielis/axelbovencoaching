9bbb99e7a13b104c2378df270ab7a5d8a0ab66ce
Sam Gielis <samgielis@gmail.com>
2023-05-12 20:35:33 +0200

    Revamp newsletter widget to be audios widget

--- src/components/NewsLetterWidget/index.tsx
+++ src/components/NewsLetterWidget/index.tsx
@@ -2,6 +2,7 @@ import { CloseIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
+  Flex,
   Heading,
   IconButton,
   SlideFade,
@@ -11,15 +12,10 @@ import {
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 import { INSIGHT_AUDIOS } from "../../routes";
-import { NewsLetterModal } from "../NewsLetterModal";
+import { AppLink } from "../AppLink";
 
 export const NewsLetterWidget = () => {
   const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: false });
-  const {
-    isOpen: isModalOpen,
-    onOpen: onOpenModal,
-    onClose: onCloseModal,
-  } = useDisclosure();
 
   const isInsightAudios = useLocation().pathname === INSIGHT_AUDIOS;
 
@@ -32,32 +28,42 @@ export const NewsLetterWidget = () => {
     }, 2000);
   }, [onOpen, isInsightAudios]);
 
+  // Af en toe een inzicht in je inbox?
   return (
-    <>
-      <Box pos="fixed" right={3} bottom={3}>
-        <SlideFade in={isOpen}>
-          <Stack bg="themeGreen.500" p={4} pr={6}>
-            <Heading color="whit" fontSize={"2xl"}>
-              Af en toe een inzicht in je inbox?
-            </Heading>
-            <Button colorScheme={"whiteAlpha"} onClick={onOpenModal}>
-              Schrijf je in op mijn nieuwsbrief!
-            </Button>
-            <IconButton
-              onClick={onClose}
-              pos="absolute"
+    <Box pos="fixed" right={3} bottom={3}>
+      <SlideFade in={isOpen}>
+        <Stack bg="themeGreen.500" p={4} pr={6}>
+          <Heading color="whit" fontSize={"2xl"} textAlign="center">
+            Je leven op autopilot doorbreken?
+          </Heading>
+          <Button colorScheme={"whiteAlpha"} onClick={onClose}>
+            <AppLink to={INSIGHT_AUDIOS}>
+              Beluister nu gratis mijn insight audio's
+            </AppLink>
+          </Button>
+          <Flex justifyContent={"center"}>
+            <Button
+              variant="ghost"
+              colorScheme="blackAlpha"
               size="xs"
-              borderRadius="0"
-              top={-2}
-              right={0}
-              colorScheme={"whiteAlpha"}
-              icon={<CloseIcon boxSize={2} />}
-              aria-label="Sluit niewusbrief widget"
-            />
-          </Stack>
-        </SlideFade>
-      </Box>
-      <NewsLetterModal isOpen={isModalOpen} onClose={onCloseModal} />
-    </>
+              onClick={onClose}
+            >
+              <AppLink to={INSIGHT_AUDIOS}>Meer leren</AppLink>
+            </Button>
+          </Flex>
+          <IconButton
+            onClick={onClose}
+            pos="absolute"
+            size="xs"
+            borderRadius="0"
+            top={-2}
+            right={0}
+            colorScheme={"whiteAlpha"}
+            icon={<CloseIcon boxSize={2} />}
+            aria-label="Sluit nieuwsbrief widget"
+          />
+        </Stack>
+      </SlideFade>
+    </Box>
   );
 };
