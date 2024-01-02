4fdd0632e6631d809449a45bfbe1b606d31ea348
Sam Gielis <samgielis@gmail.com>
2024-01-02 19:53:58 +0100

    Fix typo

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,7 +10,7 @@ export const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Axel Boven Coacing</title>
+        <title>Axel Boven Coaching</title>
         <meta
           name="description"
           content="Vrij & Verbonden Onderweg. Ik bied holistische coaching trajecten voor mensen die onrust ervaren en daar écht verandering in willen brengen door de te wandelen en vrij te bewegen in de buitenlucht."
